0cd398f94b6d32324115324ea62d61a03c3d0f71
José Hilário Alves Neto <hilaneto@yahoo.com.br>
2026-08-20 20:28:16 -0300

    mudanca de API para o Banco Central

--- indicadores/dolar.py
+++ indicadores/dolar.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from peewee import Model, AutoField, DecimalField, DateTimeField, BooleanField
 from database.conexao import db, conectar
@@ -11,7 +11,6 @@ AWESOME_API_KEY = os.getenv("AWESOME_API_KEY")
 class Dolar(Model):
     cd_dolar = AutoField()
     valor = DecimalField(max_digits=10, decimal_places=2)
-    variacao = DecimalField(max_digits=8, decimal_places=5)
     status = BooleanField(default=True, null=False)
     dt_referencia = DateTimeField(default=datetime.now, null=False)
     dt_atualizacao = DateTimeField(default=datetime.now, null=False)
@@ -22,9 +21,16 @@ class Dolar(Model):
 
     @staticmethod
     def buscar():
-        url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-        headers = { "x-api-key": AWESOME_API_KEY }
-        resposta = requests.get(url, headers=headers, timeout=10)
+        # Datas -------------------------------------------------------
+        hoje = datetime.now()
+        data_inicial = (hoje - timedelta(days=62)).strftime("%d/%m/%Y")
+        data_final = hoje.strftime("%d/%m/%Y")
+
+        # API Banco Central ---------------------------------------------
+        url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados"
+
+        parametros = { "formato": "json", "dataInicial": data_inicial, "dataFinal": data_final }
+        resposta = requests.get(url, params=parametros, timeout=10)
         resposta.raise_for_status()
         return resposta.json()
 
@@ -34,14 +40,12 @@ class Dolar(Model):
             dolar_api = dados_api["USDBRL"]
 
             dados = {"valor": dolar_api["bid"],
-                    "variacao": dolar_api["pctChange"],
                     "dt_referencia": datetime.strptime(dolar_api["create_date"],"%Y-%m-%d %H:%M:%S"),
                     "dt_atualizacao": datetime.now(),
                     "status": True}
 
         except Exception as erro:
             dados = {"valor": 0,
-                    "variacao": 0,
                     "dt_referencia": datetime.now(),
                     "dt_atualizacao": datetime.now(),
                     "status": False}
